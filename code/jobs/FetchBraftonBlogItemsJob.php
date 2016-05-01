b44ee856e2180a62d2d1ed6ee666b69abcb91516
Kirk Mayo <kirk@stripetheweb.com>
2016-05-02 11:55:47 +1200

    Moved adding news item login into service and remove old categories

--- code/jobs/FetchBraftonBlogItemsJob.php
+++ code/jobs/FetchBraftonBlogItemsJob.php
@@ -19,41 +19,7 @@ class FetchBraftonBlogItemsJob extends BuildTask {
         if ($api) {
             $newsItems = $api->getNewsHTML();
             foreach ($newsItems as $item) {
-                //TODO Move this into it's own method on BraftonService but I want to go home now
-                // so I will do it next week it's Friday now and I want a beer
-                // check if the item already exists via the ApiID or create one
-                $news = BraftonNewsItem::get()
-                    ->filter('ApiID', $item->getId())
-                    ->first();
-                if (!$news || !$news->exists()) {
-                    $news = new BraftonNewsItem();
-                    $news->ApiID = $item->getId();
-                    $news->CreatedDate = $item->getCreatedDate();
-                } else if ($news->LastModifiedDate == $item->getLastModifiedDate()) {
-                    continue;
-                }
-                // update fields
-                $news->PublishDate = $item->getPublishDate();
-                $news->CreatedDate = $item->getCreatedDate();
-                $news->LastModifiedDate = $item->getLastModifiedDate();
-                $news->Headline = $item->getHeadline();
-                $news->HtmlTitle = $item->getHtmlTitle();
-                $news->HtmlMetaDescription = $item->getHtmlMetaDescription();
-                $news->Content = $item->getText();
-                //TODO remove relations that are no longer needed
-                foreach ($item->getCategories() as $category) {
-                    $newsCategory = BraftonNewsCategory::get()
-                        ->filter('CategoryID', $category->getId())
-                        ->first();
-                    if (!$newsCategory || !$newsCategory->exists()) {
-                        $newsCategory = new BraftonNewsCategory();
-                        $newsCategory->CategoryID = $category->getId();
-                        $newsCategory->Title = $category->getName();
-                        $newsCategory->write();
-                    }
-                    $news->Categories()->add($newsCategory);
-                }
-                $news->write();
+                $service->addNews($item);
             }
         }   
     }   
--- code/service/BraftonService.php
+++ code/service/BraftonService.php
@@ -17,4 +17,49 @@ class BraftonService extends RestfulService {
     public function getApi() {
         return $this->api;
     }
+
+    /**
+     * Check if a news item already exists in the Dataobject
+     * and adds it if it does not exist
+     */
+    public function addNews($item) {
+        // check if the item already exists via the ApiID or create one
+        $news = BraftonNewsItem::get()
+            ->filter('ApiID', $item->getId())
+            ->first();
+        if (!$news || !$news->exists()) {
+            $news = new BraftonNewsItem();
+            $news->ApiID = $item->getId();
+            $news->CreatedDate = $item->getCreatedDate();
+        } else if ($news->LastModifiedDate == $item->getLastModifiedDate()) {
+            continue;
+        }
+        // update fields
+        $news->PublishDate = $item->getPublishDate();
+        $news->CreatedDate = $item->getCreatedDate();
+        $news->LastModifiedDate = $item->getLastModifiedDate();
+        $news->Headline = $item->getHeadline();
+        $news->HtmlTitle = $item->getHtmlTitle();
+        $news->HtmlMetaDescription = $item->getHtmlMetaDescription();
+        $news->Content = $item->getText();
+        $categoryList = array();
+        foreach ($item->getCategories() as $category) {
+            $categoryList[] = $category->getId();
+            $newsCategory = BraftonNewsCategory::get()
+                ->filter('CategoryID', $category->getId())
+                ->first();
+            if (!$newsCategory || !$newsCategory->exists()) {
+                $newsCategory = new BraftonNewsCategory();
+                $newsCategory->CategoryID = $category->getId();
+                $newsCategory->Title = $category->getName();
+                $newsCategory->write();
+            }
+            $news->Categories()->add($newsCategory);
+        }
+        // removed old categories no longer on the news item
+        foreach ($news->Categories()->exclude('CategoryID', $categoryList) as $delete) {
+            $news->Categories()->remove($delete);
+        }
+        $news->write();
+    }
 }
